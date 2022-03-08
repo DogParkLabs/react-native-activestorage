52efd40ce8a6949241ffea448238006172505226
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2022-03-08 12:40:52 -0600

    Decode the uri path before attempting to access the file

--- src/lib/checksum.ts
+++ src/lib/checksum.ts
@@ -2,7 +2,8 @@ import ReactNativeBlobUtil from 'react-native-blob-util';
 import { btoa } from 'abab';
 
 const checksum = async ({ path }: { path: string }) => {
-  const md5 = await ReactNativeBlobUtil.fs.hash(path, 'md5');
+  const decodedPath = decodeURI(path);
+  const md5 = await ReactNativeBlobUtil.fs.hash(decodedPath, 'md5');
   const hexArray = md5.replace(/\r|\n/g, "")
     .replace(/([\da-fA-F]{2}) ?/g, "0x$1 ")
     .replace(/ +$/, "")
